08fc2c9af3cf5fa0f3ec9c4235ef9581151acc75
muhammad.akbar <muhammad.akbar@pf.com.pk>
2025-05-27 11:47:00 +0500

    first name and lastname field added

--- models/User.js
+++ models/User.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: String,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    password: String,
-    is_admin: { type: Boolean, default: false },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false }, // camelCase
   },
   { timestamps: true }
 );
