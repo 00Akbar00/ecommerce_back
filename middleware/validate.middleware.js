4193538a4deb8df2921cc8bedf56a929ed4ef5ce
00Akbar00 <ma341604@gmail.com>
2025-06-01 16:25:09 +0500

    refactor validation middleware to export directly and adjust error status code

--- middleware/validate.middleware.js
+++ middleware/validate.middleware.js
@@ -1,9 +1,9 @@
 const { validationResult } = require("express-validator");
 
-const handleValidationErrors = (req, res, next) => {
+module.exports = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
+    return res.status(400).json({
       success: false,
       message: "Validation failed",
       errors: errors.array(),
@@ -11,5 +11,3 @@ const handleValidationErrors = (req, res, next) => {
   }
   next();
 };
-
-module.exports = handleValidationErrors;
