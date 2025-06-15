6cdb1b6abf34d45ef0868bd0896cbaf42a261718
00Akbar00 <ma341604@gmail.com>
2025-06-15 05:31:09 +0500

    refactor: remove unnecessary comment from isAdmin field in user schema

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     lastName: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false }, // camelCase
+    isAdmin: { type: Boolean, default: false }, 
   },
   { timestamps: true }
 );
